41fbc279bc38f0bd2573a3414d9290bdb4bd41b2
maxheerdegen <96018857+maxheerdegen@users.noreply.github.com>
2024-04-16 23:10:35 +0900

    add button to choose number of squares

--- javascript.js
+++ javascript.js
@@ -1,12 +1,26 @@
-const container = document.querySelector(".container");
+function chooseSquares() {
+    let numberOfSquares = +prompt("Number of squares per side? (max. 100)")
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    setSquares(numberOfSquares);
+}
 
-for (let index = 1; index <= (16 * 16); index++) {
+function setSquares(numberOfSquares) {
+    let squareMeasures = `${640/numberOfSquares}px`;
+
+    for (let index = 1; index <= (numberOfSquares ** 2); index++) {
     const square = document.createElement("div");
-    square.style.width = "40px"
-    square.style.height = "40px"
+    square.style.width = squareMeasures;
+    square.style.height = squareMeasures;
     square.classList.add("square");
     square.addEventListener("mouseover", () => {
         square.style.backgroundColor = "black";
     })
     container.appendChild(square);
+    }
 }
+
+const container = document.querySelector(".container");
+setSquares(16);
+
